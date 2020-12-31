a08c3ff262b4aadc76f2a83e1a49a6e6c64d0017
qing xiao <tinaxiao@cn.ibm.com>
2020-12-31 08:15:04 +0000

    updated port message

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
 
   
 const welcomeMessage = "Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service. Test Change!!!";
-const portMessage = "Application Running on port";
+const portMessage = "Application Running on port. Change port message.";
 
 const getWelcomeMessage = () => {
     return welcomeMessage;
